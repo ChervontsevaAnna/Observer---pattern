8a1d6177f0c57064f66083f54bef01d22bd89720
Даниил <kourin.daniil@gmail.com>
2022-03-21 14:33:29 +0300

    Реализовать диаграмму классов (стр. 86). Классы наблюдатели имеют поле счетчик, который инкрементируется каждый раз, когда вызывается метод update. Реализовала сценарий

---
+++ Observer/patternObserver/ConcreteSubject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public class ConcreteSubject : Subject
+    {
+        private List<Observer> people = new List<Observer>();
+        public void notifyObservers()
+        {
+            foreach (var i in people)
+            {
+                i.update();
+            }
+        }
+
+        public void registerObserver(Observer observer)
+        {
+            people.Add(observer);
+        }
+
+        public void removeObserver(Observer observer)
+        {
+            people.Remove(observer);
+        }
+        string state;
+        public string State
+        {
+            get { return state; }
+            set { state = value; notifyObservers(); }
+        }
+    }
+}
